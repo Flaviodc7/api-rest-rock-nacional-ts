e26114f8140529ec68e98001bca78d2a099283ff
Flavio Coscarella <flaviodc@live.com.ar>
2023-07-21 23:54:12 -0300

    Updating app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import { router } from './routes'
+import db from "./config/mongo"
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors())
+app.use(express.json())
+app.use(router);
+db().then(() => console.log("Connection Ready"))
 
 app.listen(PORT, () => console.log(`Listening in PORT: ${PORT}`))
