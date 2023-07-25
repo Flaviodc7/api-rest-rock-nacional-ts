4c6d14c416d2074ebb448c07dd808ec1346ed03b
Flavio Coscarella <flaviodc@live.com.ar>
2023-07-25 00:11:11 -0300

    Updating controllers

--- src/controllers/artist.ts
+++ src/controllers/artist.ts
@@ -1,41 +1,56 @@
 import { Request, Response } from "express"
 import { handleHttp } from '../utils/error.handle'
+import { 
+    insertArtist, getArtists as getArtistsService,
+    getArtist as getArtistService, 
+    updateArtist as updateArtistService, 
+    deleteArtist as deleteArtistService 
+} from "../services/artist.service";
 
-const getArtist = (req: Request, res: Response) => {
+const getArtist = async ({params}: Request, res: Response) => {
     try {
-
+        const { id } = params;
+        const response = await getArtistService(id);
+        const data = response ? response : "ARTIST_NOT_FOUND";
+        res.send(response);
     } catch (e) {
         handleHttp(res, "ERROR_COULDN'T_GET_ARTIST");
     }
 }
 
-const getArtists = (req: Request, res: Response) => {
+const getArtists = async (req: Request, res: Response) => {
     try {
-
+        const response = await getArtistsService();
+        res.send(response);
     } catch (e) {
         handleHttp(res, "ERROR_COULDN'T_GET_ARTISTS");
     }
 }
 
-const updateArtist = (req: Request, res: Response) => {
+const updateArtist = async ({params, body}: Request, res: Response) => {
     try {
-
+        const { id } = params;
+        const response = await updateArtistService(id, body);
+        res.send(response);
     } catch (e) {
         handleHttp(res, "ERROR_COULDN'T_UPDATE_ARTIST");
     }
 }
 
-const postArtist = (req: Request, res: Response) => {
+const postArtist = async ({ body }: Request, res: Response) => {
     try {
-
+        const responseArtist = await insertArtist(body);
+        res.send(responseArtist);
     } catch (e) {
-        handleHttp(res, "ERROR_COULDN'T_POST_ARTIST");
+        handleHttp(res, "ERROR_COULDN'T_POST_ARTIST", e);
     }
 }
 
-const deleteArtist = (req: Request, res: Response) => {
+const deleteArtist = async ({params}: Request, res: Response) => {
     try {
-
+        const { id } = params;
+        const response = await deleteArtistService(id);
+        res.send(response);
     } catch (e) {
         handleHttp(res, "ERROR_COULDN'T_DELETE_ARTIST");
     }
